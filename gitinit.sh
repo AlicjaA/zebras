7f8e5bf7626abad74bb30478c5f61ed5615d24fd
Ala <alicja.anszpergier@gmail.com>
2020-08-27 12:29:41 +0200

    changes

--- gitinit.sh
+++ gitinit.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd zebras
 git init
--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 git config --global user.email "alicja.anszpergier@gmail.com"
 git config --global user.name "AlicjaA"
 git add -A
 git commit -m 'weights from colab'
-git config remote.origin.url 'https://905a78dea0e07125a0d24b88c90262b5f2a25eb5@github.com/AlicjaA/zebras.git'
+git config remote.origin.url 'https://f225c6305423cb356dfcebff5506232cdd7e76c1@github.com/AlicjaA/zebras.git'
 git push -u weights master
