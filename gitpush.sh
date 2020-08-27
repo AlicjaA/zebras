a8814baf447626cd13c6fc951acf5fb6c0d41463
Ala <alicja.anszpergier@gmail.com>
2020-08-27 19:34:58 +0200

    ch

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ git config --global user.email "alicja.anszpergier@gmail.com"
 git config --global user.name "AlicjaA"
 git add -A
 git commit -m 'weights from colab'
-git config remote.origin.url 'https://354dc29a4e3bdf82226886d10e0637952c79dc42@github.com/AlicjaA/zebras.git'
+#git config remote.origin.url 'https://354dc29a4e3bdf82226886d10e0637952c79dc42@github.com/AlicjaA/zebras.git'
 git push -u origin master
